226b804f657c39a6261536de1083d029ce007b86
David <davidm1795@gmail.com>
2024-10-24 15:20:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class ScannerImport {
                         const x = document.getElementById('list'); 
                     const l = document.createElement('li');
       
-                        l.innerText = '' + e.preventDefault + '';
+                        l.innerText = '' + e.defaultPrevented + '';
       
                       x.appendChild(l);
                 }
